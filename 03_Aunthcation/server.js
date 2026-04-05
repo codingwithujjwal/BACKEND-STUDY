9350d20c34fc351170c6ebdf35990e16712d8d79
Ujjwal Nandi <ujjwalnandi767@gmail.com>
2026-04-05 20:27:15 +0530

    feat: initialize authentication module with Express and MongoDB connection
    - Added package.json to manage dependencies including dotenv, express, and mongoose.
    - Created server.js to set up the Express server and connect to the database.
    - Implemented app.js to configure the Express application.
    - Developed db.js for MongoDB connection handling.

--- 03_Aunthcation/server.js
+++ 03_Aunthcation/server.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const app = require("./src/app");
-const connectedToDB = require("./src/db/db");
+const connectDb=require("./src/db/db")
 
-connectedToDB();
+
+
+connectDb();
+
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
---
+++ 03_Aunthcation/src/db/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+async function connectDb() {
+    try {
+
+      await  mongoose.connect(process.env.MONGO_URI)
+        console.log("Database connected successfully")
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
+
+   
+module.exports=connectDb
