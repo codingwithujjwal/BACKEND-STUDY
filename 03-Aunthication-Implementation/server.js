d0b52918df8713f9dfc404bc5f8b3b44f39d634e
Ujjwal Nandi <ujjwalnandi767@gmail.com>
2026-04-06 20:10:36 +0530

    feat: implement authentication module with user registration
    - add package.json for project dependencies
    - create server.js to initialize the application
    - set up Express app in app.js with JSON parsing and authentication routes
    - implement user registration logic in auth.controller.js
    - establish MongoDB connection in db.js
    - define user model in user.model.js
    - create authentication routes in auth.routes.js

--- 03-Aunthication-Implementation/server.js
+++ 03-Aunthication-Implementation/server.js
@@ -1,10 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
 const app = require("./src/app");
-const connectDb=require("./src/db/db")
+const connectDb = require("./src/db/db");
 
 
 
 connectDb();
+
+
 
 
 app.listen(3000, () => {
